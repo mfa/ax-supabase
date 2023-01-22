b558fa9c5b530472371180f483e6e12937206d10
Andreas Madsack <andreas@madflex.de>
2023-01-22 19:09:03 +0100

    remove not needed line

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@ def save(dataset):
     url = os.environ.get("SUPABASE_URL")
     key = os.environ.get("SUPABASE_KEY")
     supabase = create_client(url, key)
-    r = supabase.table("generated").select("*").execute()
     data = supabase.table("generated").insert(dataset).execute()
     app.logger.info(data)
 
